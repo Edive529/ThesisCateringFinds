da358ac0d16dd50b113a6956bdb665e8f7c1724b
kylevillz <93573554+kylevillz@users.noreply.github.com>
2023-06-10 11:00:53 +0800

    added restaurant infos at left side

--- cateringMaps.php
+++ cateringMaps.php
@@ -1,31 +1,34 @@
-<?php 
-include("db/dbb.php"); 
-        
+<?php
+include 'connectdb.php';
+
+session_start();
+
 include_once 'navbar.php';
- 
 
- 
-if(isset($_POST['submit_add'])){
-    $map_add = $_POST['imap_add'];
-    $lat = $_POST['ilat'];
-    $longi = $_POST['ilongi'];
 
-         $sql = "INSERT INTO mapsamp (map_add, lat, longi) VALUES ('$map_add', '$lat', '$longi')"; 
-        if(mysqli_query($connection, $sql)) {
 
-        echo "<script> alert('Location successfully Added'); window.location='mapsamp.php';  </script>"; 
-        } else {
-       echo "<script>alert('Invalid Input. Please Try Again.'); window.location='mapsamp.php';  </script>"; 
-        }
-}
+// if(isset($_POST['submit_add'])){
+//     $map_add = $_POST['imap_add'];
+//     $lat = $_POST['ilat'];
+//     $longi = $_POST['ilongi'];
+//
+//          $sql = "INSERT INTO mapsamp (map_add, lat, longi) VALUES ('$map_add', '$lat', '$longi')";
+//         if(mysqli_query($connection, $sql)) {
+//
+//         echo "<script> alert('Location successfully Added'); window.location='mapsamp.php';  </script>";
+//         } else {
+//        echo "<script>alert('Invalid Input. Please Try Again.'); window.location='mapsamp.php';  </script>";
+//         }
+// }
 
 require('locations.php');
 $samp = new locations;
 $allData = $samp->getLocations();
 $allData = json_encode($allData, true);
-echo '<div id="showData">' . $allData. '</div>';  
+echo '<div id="showData">' . $allData. '</div>';
 
- 
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -37,9 +40,9 @@ echo '<div id="showData">' . $allData. '</div>';
 
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 
-    <script type="text/javascript" src="bootstrap/js/googlemaps.js"></script> 
+    <script type="text/javascript" src="bootstrap/js/googlemaps.js"></script>
 
-   
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
    <!-- Favicon -->
@@ -49,7 +52,7 @@ echo '<div id="showData">' . $allData. '</div>';
    <link rel="preconnect" href="https://fonts.googleapis.com">
    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Inter:wght@700;800&display=swap" rel="stylesheet">
-   
+
    <!-- Icon Font Stylesheet -->
    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
@@ -80,14 +83,14 @@ echo '<div id="showData">' . $allData. '</div>';
 
     <title>maps</title>
 </head>
-<body> 
+<body>
         <div class="container">
         <div class="row" >
 
-        <div class="container col  overflow-auto"> 
+        <div class="container col  overflow-auto">
 
         <!-- <form action="" method="POST">
-            <p> 
+            <p>
                 <input type="text" name="imap_add" placeholder="enter address" required> <br>
                 <input type="text" name="ilat" placeholder="enter Latitude" required>
                 <input type="text" name="ilongi" placeholder="enter Longitude" required>
@@ -95,51 +98,42 @@ echo '<div id="showData">' . $allData. '</div>';
             <input type="submit" name="submit_add">
 
         </form> -->
-      
 
 
-        <div class=" py-2 m-2"> 
-           
+
+        <div class=" py-2 m-2">
+
+          <?php
+             $select_products = $pdo->prepare("SELECT * FROM `tbl_user` where status = 'approved'");
+             $select_products->execute();
+             if($select_products->rowCount() > 0){
+                while($fetch_prodcut = $select_products->fetch(PDO::FETCH_ASSOC)){
+          ?>
+
+
+
+
+
             <div class="  wow fadeInUp" data-wow-delay="0.1s">
                 <a class="cat-item rounded p-4" href="">
                     <i class="fa fa-3x fa-mail-bulk text-primary"></i>
-                    <h6 class="mb-3">Restaurant 1</h6>
-                    <p class="mb-0">123 Address</p>
+                    <h6 class="mb-3"><?= $fetch_prodcut['restaurant']; ?></h6>
+                    <p class="mb-0"><?= $fetch_prodcut['address']; ?></p>
                 </a>
             </div>
-            <div class="  wow fadeInUp" data-wow-delay="0.1s">
-                <a class="cat-item rounded p-4" href="">
-                    <i class="fa fa-3x fa-mail-bulk text-primary"></i>
-                    <h6 class="mb-3">Restaurant 1</h6>
-                    <p class="mb-0">123 Address</p>
-                </a> 
-            </div>
-            <div class="  wow fadeInUp" data-wow-delay="0.1s">
-                <a class="cat-item rounded p-4" href="">
-                    <i class="fa fa-3x fa-mail-bulk text-primary"></i>
-                    <h6 class="mb-3">Restaurant 1</h6>
-                    <p class="mb-0">123 Address</p>
-                </a> 
-            </div>
-            <div class="  wow fadeInUp" data-wow-delay="0.1s">
-                <a class="cat-item rounded p-4" href="">
-                    <i class="fa fa-3x fa-mail-bulk text-primary"></i>
-                    <h6 class="mb-3">Restaurant 1</h6>
-                    <p class="mb-0">123 Address</p>
-                </a> 
-            </div>
-            
-        </div> 
+        <?php }} ?>
+
+        </div>
        </div>
-       
-      
-        <div class="col-lg-9 "> 
-            <div id="map"></div>  
-        </div>  
+
+
+        <div class="col-lg-9 ">
+            <div id="map"></div>
+        </div>
 
         </div>
     </div>
-    
+
     <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAif965hDy98LYDXjMrGN9-U3BlqMhZvjc&callback=initMap"></script>
        <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
@@ -151,10 +145,10 @@ echo '<div id="showData">' . $allData. '</div>';
 
     <!-- Template Javascript -->
     <script src="bootstrap/js/main.js"></script>
-    
+
      </body>
 
-      
+
 </html>
 <?php
 
