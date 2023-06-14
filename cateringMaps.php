9d2437867a5e2e74c70f0e7875c24d4808b9d7d3
Edive529 <dave.mosqueda@g.msuiit.edu.ph>
2023-06-14 13:19:37 +0800

    infowindow now has image and description

--- cateringMaps.php
+++ cateringMaps.php
@@ -40,7 +40,7 @@ echo '<div id="showData">' . $allData. '</div>';
 
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 
-    <script type="text/javascript" src="bootstrap/js/googlemaps.js"></script>
+    
 
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -147,6 +147,7 @@ echo '<div id="showData">' . $allData. '</div>';
 
     <!-- Template Javascript -->
     <script src="bootstrap/js/main.js"></script>
+    <script type="text/javascript" src="bootstrap/js/googlemaps.js"></script>
 
      </body>
 
