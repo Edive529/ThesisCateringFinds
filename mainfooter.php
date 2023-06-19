889bb68f180ca3944135ead5d6aac025fafc3770
kylevillz <93573554+kylevillz@users.noreply.github.com>
2023-06-19 10:02:45 +0800

    edited footer

--- mainfooter.php
+++ mainfooter.php
@@ -66,7 +66,7 @@
 
                     <div class="col-lg-4 col-md-6">
                         <h5 class="text-white mb-4">Newsletter</h5>
-                        <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
+                        <p></p>
                         <div class="position-relative mx-auto" style="max-width: 400px;">
                             <input class="form-control bg-transparent w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
                             <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
@@ -78,7 +78,7 @@
                 <div class="copyright">
                     <div class="row">
                         <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                            &copy; <a class="border-bottom" href="index.php">CateringFinds</a>, All Right Reserved. 
+                            &copy; <a class="border-bottom" href="index.php">CateringFinds</a>, All Right Reserved.
 
                         </div>
                         <div class="col-md-6 text-center text-md-end">
